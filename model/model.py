480a2e84699d36ad067f305c6625b075223c55a0
XiaoLiu <1718113299@qq.com>
2026-09-18 19:50:54 +0800

    实现MLP多层感知机，即前馈网络FFN

--- model/model.py
+++ model/model.py
@@ -319,5 +319,27 @@ class Attention(nn.Module):
         output=self.resid_dropout(self.o_proj(output))
         return output,past_kv 
 
-           
+class FeedForward(nn.Module):
+    def __init__(self,config:MiniMindConfig):
+        super().__init__()
+        if config.intermediate_size is None:
+            intermediate_size=int(config.hidden_size*8/3)
+            config.intermediate_size=64*((intermediate_size+64-1)//64)
 
+            self.gate_proj=nn.Linear(
+                config.hidden_size,config.intermediate_size,bias=False
+            )
+            self.down_proj=nn.Linear(
+                config.intermediate_size,config.hidden_size,bias=False
+            )
+            self.up_proj=nn.Linear(
+                config.hidden_size,config.intermediate_size,bias=False
+            )
+            self.dropout=nn.Dropout(config.dropout)
+            self.act_fn=ACT2FN[config.hidden_act]
+
+    def forward(self,x):
+        gated=self.act_fn(self.gate_proj(x))*self.up_proj(x)
+        return self.dropout(self.down_proj(gated))
+
+    
